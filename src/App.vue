eebbd359fa82f15983e23868e69c024ced46e0b6
tzu-ying666 <1051247148@nkust.edu.com>
2021-02-23 16:04:42 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,63 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+<template>   <!--html -->
+  <div id = "app">
+    <h1>ToDo List</h1>
+    <img alt = "Vue logo" src="./assets/logo.png">
+    <!-- 
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    -->
+    <p>{{ message }}</p>
+    <input v-model = "message">
+    <button type="button" v-on:click="buttonClick">Button</button>
+    <br>
+    <a>{{arr}}</a>
+    <div class = "list">
+      <ul>
+        <li v-for="item in arr" :key="item">{{ item }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script>    //js
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    // HelloWorld
+  },  //找文件
+  
+  data() { 
+    return {
+      message: '' ,
+      recordList: '',
+      arr: [1,2,3,4,5,6],
+    }
+  },
+  watch:{
+    // info(x){
+    // }
+  },
+  computed: {},
+  methods: {
+    buttonClick () {  //buttonClick: function ()
+      const addTodo = this.message;
+      this.recordList = addTodo;
+
+      this.arr.push(addTodo);
+
+      this.message = '';
+      
+      console.log(addTodo);
+    }
+  },
+  created() {},
+
 }
+
 </script>
 
-<style>
+<style>  /*css*/
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +65,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.list{
+  width:1000px;
+  height:1000px;
+  display: flex;
 }
 </style>
